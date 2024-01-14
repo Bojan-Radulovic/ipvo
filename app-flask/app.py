9af7a39fd512919782e088e30c294a712cf241be
Bojan-Radulovic <bokiflet@gmail.com>
2024-01-15 00:12:36 +0100

    Phase 1: Basic React frontend and Flask API

---
+++ app-flask/app.py
@@ -0,0 +1,25 @@
+# Filename - app.py
+
+# Import flask and datetime module for showing date and time
+from flask import Flask
+import datetime
+
+x = datetime.datetime.now()
+
+# Initializing flask app
+application = Flask(__name__)  # Change 'app' to 'application'
+
+# Route for seeing a data
+@application.route('/app-flask/data')
+def get_time():
+    # Returning an api for showing in reactjs
+    return {
+        'Name': "geek",
+        "Age": "22",
+        "Date": x,
+        "programming": "python"
+    }
+
+# Running app
+if __name__ == '__main__':
+    application.run(debug=True, host='0.0.0.0')
